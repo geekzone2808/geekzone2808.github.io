a61032fd52efc95c76480b5b8d08bbf9980d0d57
Studentplace98 <45861531+Studentplace98@users.noreply.github.com>
2019-06-05 12:51:22 -0500

    n

--- Subdominios/CineySeriesPlus/js/cookieManager.js
+++ Subdominios/CineySeriesPlus/js/cookieManager.js
@@ -26,5 +26,5 @@ document.getElementById("frame").setAttribute("height",altura);
 
 function setCookie(video,altura){
 	document.cookie = 'video='+video+';  domain=cineyseriesplus.blogspot.com ; path=/p/'; 
-	document.cookie = 'altura='+altura+'px; domain=cineyseriesplus.blogspot.com ;path=/p/';" 	
+	document.cookie = 'altura='+altura+'px; domain=cineyseriesplus.blogspot.com ;path=/p/'; 	
 }
